6196044ff1a2f750882cd7ce5bcf23620dfe99b5
Geilson <geilsonfidelis@gmail.com>
2020-06-04 18:07:33 -0300

    Definindo aleatoriedade do bax de alimentação, assim, aumentando o tamanho da snake

--- js/script.js
+++ js/script.js
@@ -60,6 +60,13 @@ function iniciarJogo(){
 	if(direction == "up") snakeY -= box;
 	if (direction == "down") snakeY += box;
 
+	if(snakeX != food.x || snakeY != food.y){
+		snake.pop();
+	}
+	else{food.x = Math.floor(Math.random() * 15 + 1) * box;
+		 food.y = Math.floor(Math.random() * 15 + 1) * box;		
+	}
+
 	snake.pop();
 
 	let newHead = {
