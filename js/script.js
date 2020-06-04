04bed57401970985c5f698e278fc3bcd67d4dd75
Geilson <geilsonfidelis@gmail.com>
2020-06-04 17:59:18 -0300

    Criando box que alimenta a snake

--- js/script.js
+++ js/script.js
@@ -8,6 +8,10 @@ snake[0] = {
 }
 
 let direction = "right";
+let food = {
+	x: Math.floor(Math.random() * 15 + 1) * box,
+	y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
 	context.fillStyle = "lightgreen";
@@ -20,6 +24,12 @@ function criarCobrinha(){
 		context.fillRect(snake[i].x, snake[i].y, box, box);
 	}
 }
+
+function drawFood(){
+	context.fillStyle = "brown";
+	context.fillRect(food.x, food.y, box, box, box);
+}
+
 
 document.addEventListener('keydown' update);
 
@@ -39,6 +49,7 @@ function iniciarJogo(){
 
 	criarBG();
 	criarCobrinha();
+	drawFood();
 
 
 	let snakeX = snake[0].x;
