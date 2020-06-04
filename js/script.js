4ff3fac15f2f4ba0c9f0b998e8828930ee094002
Geilson <geilsonfidelis@gmail.com>
2020-06-04 16:51:11 -0300

    defining snake moviment

--- js/script.js
+++ js/script.js
@@ -7,17 +7,42 @@ snake[0] = {
 	y: 8 * box
 }
 
+let direction = "right";
+
 function criarBG() {
 	context.fillStyle = "lightgreen";
 	context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha(){
-	for(i=0; i sanke.length; i++){
+	for(i=0; i < snake.length; i++){
 		context.fillStyle = "green";
-		context.fillRect(sanke[i].x, snake[i].y, box, box);
+		context.fillRect(snake[i].x, snake[i].y, box, box);
 	}
 }
 
-criarBG ();
-criarCobrinha();
+function iniciarJogo(){
+	criarBG();
+	criarCobrinha();
+
+
+	let snakeX = snake[0].x;
+	let snakeY = snake[0].y;
+
+	if(direction == "right") snakeX += box;
+	if(direction == "left") snakeX -= box;
+	if(direction == "up") snakeY -= box;
+	if (direction == "down") snakeY += box;
+
+	snake.pop();
+
+	let newHead = {
+		x: snakeX, 
+		y: snakeY
+	}
+	
+	snake.unshift(newHead);
+	
+}
+
+let jogo = setIterval(iniciarJogo, 100);
